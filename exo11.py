826d2e92cea794970fd3198b7e7e583984fb888c
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-02-11 09:40:22 +0100

    exo 11 correction

--- exo11.py
+++ exo11.py
@@ -1,11 +1,12 @@
 def validate_word(string):
     already_use = []
     for char in string:
-        if char in already_use:
+        if char.lower() in already_use:
             return False
         else:
-            already_use.append(char)
+            already_use.append(char.lower())
     return True
 
 print(validate_word("test"))
 print(validate_word("Esteban"))
+print(validate_word("This"))
