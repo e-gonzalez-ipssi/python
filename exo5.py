002e2468cb18c8b356f50f286a4ddef3b06d45d4
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-02-11 10:43:26 +0100

    exo 5

---
+++ exo5.py
@@ -0,0 +1,9 @@
+def pillars(num, length, width):
+    if num <= 0:
+        return 0
+    else:
+        return (num-1) * (length * 100) + (num-2) * (width)
+
+print(pillars(1, 10, 10))
+print(pillars(2, 20, 25))
+print(pillars(11, 15, 30))
