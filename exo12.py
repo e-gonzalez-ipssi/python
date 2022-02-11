0c2187527da9899a7e9e9fd5411888f0ba182622
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-02-11 10:21:44 +0100

    correction 12

--- exo12.py
+++ exo12.py
@@ -1,5 +1,7 @@
 def merge_string(word1, word2, letter):
-    return word1.split(letter)[0] + letter + word2.split(letter)[1]
+    return word1.split(letter)[0] + letter + word2.lstrip(word2.split(letter)[0]+letter)
 
 print(merge_string("Hello", "World", "l"))
-print(merge_string("Esteban", "James", "a"))
+print(merge_string("coding", "anywhere", "n"))
+print(merge_string("jason", "samson", "s"))
+print(merge_string("wonderful", "people", "e"))
