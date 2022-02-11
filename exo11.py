933ddf959e56cf44e7343c497484da0f4266ef89
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-02-11 10:37:21 +0100

    exo 11 : j'avais mal lu l'éxercie

--- exo11.py
+++ exo11.py
@@ -1,11 +1,27 @@
 def validate_word(string):
-    already_use = []
+    already_use = {}
     for char in string:
         if char in already_use:
-            return False
+            already_use[char] += 1
         else:
-            already_use.append(char)
+            already_use[char] = 1
+    
+    nums = []
+    for key in already_use:
+        nums.append(already_use[key])
+
+    for i in range(len(nums)):
+        if nums[i] != nums[0]:
+            return False
     return True
 
-print(validate_word("test"))
-print(validate_word("Esteban"))
+print(validate_word("abcabc"))
+print(validate_word("Abcabc"))
+print(validate_word("AbcabcC"))
+print(validate_word("AbcCBa"))
+print(validate_word("pzppz"))
+print(validate_word("?!?!?!"))
+print(validate_word("abc123"))
+print(validate_word("abcabcd"))
+print(validate_word("abc!abc!"))
+print(validate_word("abc:abc"))
